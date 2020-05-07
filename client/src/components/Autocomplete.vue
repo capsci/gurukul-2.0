79df313e60ba29b422f100c7739ac9eb87990e31
Kapil <kapilsomani.h@gmail.com>
2020-05-07 01:09:40 -0400

    Autocomplete dropdown using VueJS v-autocomplete tag

---
+++ client/src/components/Autocomplete.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <v-autocomplete
+            v-model="select"
+            :items="items"
+            item-text="itemtext"
+            :label="label"
+            filled
+            return-object>
+            <div
+                slot="prepend-item"
+                class="grey--text">
+                <!--
+                    TODO: This number should reflect number of filtered options
+                    {{ items.length }} available options
+                -->
+            </div>
+            <template slot="selection" slot-scope="{ item }">
+                <slot :item="item" name="selectionTemplate"></slot>
+            </template>
+            <template slot="item" slot-scope="{ item }">
+                <slot :item="item" name="itemTemplate"></slot>
+            </template>
+        </v-autocomplete>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Autocomplete',
+    props: {
+        additemtext: Function,
+        entries: Array,
+        label: String,
+    },
+    data: function() {
+        return {
+            select: null,
+            search: null,
+        }
+    },
+    computed: {
+        items: function() {
+            return this.entries.map(entry => {
+                var itemtext = this.additemtext(entry);
+                return Object.assign({}, entry, { itemtext })
+            });
+        }
+    },
+    watch: {
+        search: function() {
+            if (this.items.length > 0) return
+        },
+    },
+}
+</script>
+
+<!--
+Usage:
+<Autocomplete
+    v-bind:entries="<ENTRIES_ARRAY>"
+    v-bind:label="<LABEL_FOR_DROPDOWN>"
+    v-bind:additemtext="<METHOD_TO_GENERATE_ITEM_TEXT_FOR_ENTRY>">
+    <template slot="itemTemplate" slot-scope="{ item }">
+        <DROPDOWN_ITEM_LAYOUT>
+    </template>
+    <template slot="selectionTemplate" slot-scope="{ item }">
+        <SELECTED_ITEM_LAYOUT>
+    </template>
+</Autocomplete>
+-->
