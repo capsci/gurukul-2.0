f42f1ab3f724b85c4c7a73ad1418bf1882215f06
Kapil <kapilsomani.h@gmail.com>
2020-05-28 07:45:34 -0400

    Add custom theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#482ff7',
+                secondary: '#46c3db',
+                info: '#17b978',
+                warning: '#ee5a5a',
+                error: '#ac3f21',
+                background: '#dee1ec',
+            },
+        }
+    }
 });
